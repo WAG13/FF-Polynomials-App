d364f24d524a783c21ee32dc8e51f4223c285a91
YanaSkyrda <yana.skirda101010@gmail.com>
2020-05-31 17:01:09 +0300

    Roots number function updated

--- Algebra-polynom/Algebra-polynom/Polynom/Polynom.cpp
+++ Algebra-polynom/Algebra-polynom/Polynom/Polynom.cpp
@@ -346,12 +346,16 @@ bool operator==(Polynom const& p1, Polynom const& p2)
 
 /*4     Number of roots       */
 long long Polynom::rootsNumber() {
-	long long pow = getPolyPower() + 1;
+	long long pow = getPrime() - 1;
 	Matrix AMatrix(pow, pow);
 
 	for (long long i = 0, shift = 0; i < pow; i++, shift++) {
 		for (long long j = 0; j < pow; j++) {
-			AMatrix.setElement(i, j, getTermKey((j + shift) % pow));
+		    if ( (j + shift) % pow == 0) {
+                AMatrix.setElement(i, j, (getTermKey(0) + getTermKey(pow)) % (pow + 1));
+		    } else {
+		        AMatrix.setElement(i, j, getTermKey((j + shift) % pow));
+		    }
 		}
 	}
 
